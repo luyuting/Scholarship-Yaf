763a38c7ce918bb802b2db884c295643172d997e
luyuting <1561831147@qq.com>
2016-03-29 23:30:27 +0800

    定义Ajax返回信息的常量类

---
+++ application/library/Comm/Const.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * 错误信息常量定义
+     * @author luyuting
+     *
+     */
+    class Comm_Const {
+        
+        // 代码及其解释，用逗号分隔
+        const SUCCESS = '10000';
+        
+        const E_INVALID_PARAM = '10001,Invalid Parameters';
+        const E_WRONG_ACCOUNT = '10002,No Such User';
+        const E_WRONG_PASS = '10003,Wrong Password';
+        
+        const E_UNKOWN = '10004,Unkown Error';
+    }
