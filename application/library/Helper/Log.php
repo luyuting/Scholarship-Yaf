d6d61ae305e2f865523a7319197900c059b53239
luyuting <1561831147@qq.com>
2016-04-01 16:35:37 +0800

    日志记录

--- application/library/Helper/Log.php
+++ application/library/Helper/Log.php
@@ -1,16 +1,17 @@
 <?php
     class Helper_Log {
         
-        public static function writeLog($flie, $content, $flag = FILE_APPEND) {
+        public static function writeLog($file, $content, $flag = FILE_APPEND) {
             $log_dir = realpath(APP_PATH . '/../applogs');
-            $file = $log_dir . date('Y/md') . $file . 'log';
+            $file_dir = $log_dir . date('/Y/md');
             
-            if (!is_dir($file)) {
-                mkdir($file, 0755, true);
-                chmod($file, 0755);
+            if (!is_dir($file_dir)) {
+                mkdir($file_dir, 0755, true);
+                chmod($file_dir, 0755);
             }
+            $filename = $file_dir . '/' . $file . '.log';
             $content = '[' . date('Y-m-d H:i:s') . ']' . $content . "\r\n";
-            file_put_contents($file, $content, $flag);
+            file_put_contents($filename, $content, $flag);
         }
         
     }
